36ee4bc654346e4f34166a8f1f5fa206507c3814
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-12-29 07:25:15 +0700

    add session check layout auth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,17 +1,34 @@
 import { Epilogue } from "next/font/google";
 import "../globals.css";
 
+import { Toaster } from "@/components/ui/toaster";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import { Metadata } from "next";
+
 const epilogue = Epilogue({ subsets: ["latin"] });
 
-export default function RootLayout({
+export const metadata: Metadata = {
+  viewport: "",
+};
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
+  if (session !== null) {
+    return redirect("/");
+  }
+
   return (
     <html lang="en">
       <body className={epilogue.className}>
         <main>{children}</main>
+        <Toaster />
       </body>
     </html>
   );
