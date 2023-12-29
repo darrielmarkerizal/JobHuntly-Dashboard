0d5b0f4340d7d437ab26ec30856e826410918222
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-12-29 07:30:16 +0700

    integrate header

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { PlusIcon } from "lucide-react";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import React, { FC } from "react";
 
@@ -10,13 +11,17 @@ interface headerProps {}
 const Header: FC<headerProps> = ({}) => {
   const router = useRouter();
 
+  const { data: session } = useSession();
+
+  console.log(session);
+
   const navCreateJobPage = () => router.push("/post-a-job");
 
   return (
     <div className="pb-3 mb-8 border-b border-border flex flex-row items-center justify-between">
       <div>
         <div>Company</div>
-        <div className="font-semibold ">Twitter</div>
+        <div className="font-semibold ">{session?.user.name}</div>
       </div>
       <div>
         <Button className="rounded-none py-3 px-6" onClick={navCreateJobPage}>
